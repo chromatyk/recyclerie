890f5c215e56bf7cfe4ec50faef2fd08ff350522
chromatyk <42806308+chromatyk@users.noreply.github.com>
2019-09-11 17:32:01 +0200

    init header

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <!--        <base href="https://www.villeinfo.fr" />-->
+  <!--        <meta name="generator" content="Jekyll v3.8.5" />-->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="Site de la recyclerie du pays Nayonnais" />
+  <meta name="publisher" content="Kaiartsu">
+  <meta name="author" content="Kaiartsu" />
+  <meta name="reply-to" content="contact@kaiartsu.fr">
+  <title>Accueil | Recyclerie du Pays Noyonnais | Hauts-de-France</title>
+  <!-- Link CSS -->
+  <link rel="shortcut icon" type="assets/images/logo/fav.png" href="assets/images/Logo.png"/>
+  <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+<?php
+  include 'vues/header.php';
+?>
+<div class="newsAlerte">
+
+</div>
+<div class="lastProducts">
+
+</div>
+</div>
+  <script src="assets/lib/bootstrap/js/bootstrap.js" type="text/javascript" /></script>
+  <script src="assets/js/main.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ vues/header.php
@@ -0,0 +1,33 @@
+<header class="header">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="row collapse navbar-collapse" id="navbarTogglerDemo01">
+      <div class="nav-item col-1" id="sidebar-left">
+      </div>
+    <ul class="navbar-nav col-lg-10">
+      <li class="nav-item col-2 col-md-4 col-lg-2 ">
+        <a class="nav-link" id="activePage" href="#">Accueil</a>
+      </li>
+      <li class="nav-item col-4 col-lg-2">
+        <a class="nav-link" href="#">Sensibilisation</a>
+      </li>
+      <li class="nav-item col-4 col-lg-2">
+        <a class="nav-link" href="#">Kézako</a>
+      </li>
+      <li class="nav-item col-4 col-lg-2">
+        <a class="nav-link" href="#">Boutique</a>
+      </li>
+      <li class="nav-item col-4 col-lg-2">
+        <a class="nav-link" href="#">Galerie</a>
+      </li>
+      <li class="nav-item col-4 col-lg-2">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+      <div class="nav-item col-1"  id="sidebar-right">
+      </div>
+  </div>
+</nav>
+</header>
