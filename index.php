3dceb82eb2671929c45ed996786d5ee84295485b
chromatyk <42806308+chromatyk@users.noreply.github.com>
2019-09-16 12:09:24 +0200

    home v1

--- index.php
+++ index.php
@@ -17,18 +17,123 @@
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 </head>
 <body>
-<div class="container-fluid">
-<?php
-  include 'vues/header.php';
-?>
-<div class="newsAlerte">
+  <div class="container-fluid">
+    <?php
+    include 'vues/header.php';
+    ?>
+    <div class="newsAlerte container-fluid" style="background-image:url('assets/images/news.jpg');">
+      <div class="row">
+        <div class="offset-1 col-12 col-lg-3" id="title">
+          <p>
+            Titre de la News
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="offset-4 col-1 offset-sm-7 col-sm-1 offset-md-8 col-md-1 offset-lg-9 col-lg-1">
+          <button id="buttonNews" class="fill" onclick="window.location.href='allArticles.php'">En savoir plus</button>
+        </div>
+      </div>
+    </div>
+    <div class="lastProducts">
+      <div class="row">
+        <div class="col-12">
+          <h2 id="newsProducts">Nouveautés en boutique</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div id="carousel">
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 1</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 2</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 3</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 4</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 5</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 6</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 7</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 8</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 9</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+              <img src="assets/images/objet.jpeg" class="card-img-top" alt="imag objet">
+              <div class="card-body">
+                <h3 class="card-title">Article 10</h3>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="product.php" class="btn btn-primary">Détails de l'objet</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="lastProducts">
 
-</div>
-<div class="lastProducts">
-
-</div>
-</div>
+    </div>
+  </div>
   <script src="assets/lib/bootstrap/js/bootstrap.js" type="text/javascript" /></script>
   <script src="assets/js/main.js" type="text/javascript"></script>
+  <script src="assets/js/jquery.carouFredSel-6.0.4-packed.js" type="text/javascript"></script>
 </body>
 </html>
