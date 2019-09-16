9c398d6b5f4be5306ce71be126622377f8108554
LexaeART <astheae@gmail.com>
2019-09-16 12:40:01 +0200

    produit

---
+++ vues/footer.php
@@ -0,0 +1,52 @@
+<footer>
+  <div class="container">
+
+    <div class="row">
+      <div class="col-12">
+        <h3>Restez connecté</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs12 col-sm-12 col-md-6 col-lg-4">
+        <h4>Contacter la Recyclerie</h4>
+        <hr />
+        <p>
+          La recyclerie du Pays noyonnais répond à toutes vos questions. A votre service du lundi au vendredi.
+        </p>
+        <button class="fill" onclick="window.location.href='mailto:san@antonio.net'">Contact</button>
+      </div>
+      <div class="col-xs12 col-sm-12 col-md-6 col-lg-4">
+        <h4>Liens pratiques</h4>
+        <hr />
+        <ul>
+          <li>
+            <a href="mentions_legales">Mentions Légales</a>
+          </li>
+          <li>
+            <a href="charte_de_confidencialite">Charte de confidentialité</a>
+          </li>
+          <li>
+            <a href="nous_contacter">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-xs12 col-sm-12 col-md-6 col-lg-4">
+        <h4>Suivre la Recyclerie</h4>
+        <hr />
+        <div class="row">
+          <div class="col-lg-12 socialMedia">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12" id="credit">
+        Site créé par <a href="https://www.kaiartsu.fr">Kaiartsu</a>.
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="https://kit.fontawesome.com/9d0dc13277.js"></script>
