9221a0182649e3504f8d5d6cc6ea0f1919ff630a
LexaeART <astheae@gmail.com>
2019-09-17 11:09:01 +0200

    partenaires

---
+++ partenaires.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <!--        <base href="https://www.villeinfo.fr" />-->
+  <!--        <meta name="generator" content="Jekyll v3.8.5" />-->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="Site de la recyclerie du Pays Noyonnais" />
+  <meta name="publisher" content="Kaiartsu">
+  <meta name="author" content="Kaiartsu" />
+  <meta name="reply-to" content="contact@kaiartsu.fr">
+  <title>Partenaires | Recyclerie du Pays Noyonnais | Hauts-de-France</title>
+  <!-- Link CSS -->
+  <link rel="shortcut icon" type="assets/images/favicon.png" href="assets/images/favicon.png"/>
+  <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/partenaires.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/footer.css" rel="stylesheet" type="text/css"/>
+  <link href="https://unpkg.com/webkul-micron@1.1.6/dist/css/micron.min.css" type="text/css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+  <div class="container-fluid">
+    <?php
+    include 'vues/header.php';
+    ?>
+      <!-- Fil d'ariane -->
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
+          <li class="breadcrumb-item active">Partenaires</li>
+        </ol>
+      </nav>
+      <!-- Fin du fil d'ariane -->
+      <div class="container">
+        <div id="main">
+          <div class="row">
+            <div class="col-12 préambule">
+              <h1>Nos Partenaires</h1>
+              <p>
+                Les partenaires de la recyclerie du Pays noyonnais.
+              </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/caisse_epargne.jpg" />
+            </div>
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/danone.PNG" />
+            </div>
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/dep_oise.jpg" />
+            </div>
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/europe.png" />
+            </div>
+
+
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/logo-union-europeenne.jpg" />
+            </div>
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/mlcp.jpg" />
+            </div>
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/pays_des_sources.png" />
+            </div>
+            <div class="col-2 kaiPartenaire text-center">
+              <img src="assets/images/partenaires/pole_emploi.png" />
+            </div>
+</div>
+        </div>
+      </div>
+  </div>
+  <?php
+    include 'vues/footer.php';
+  ?>
+  <script src="assets/lib/bootstrap/js/bootstrap.js" type="text/javascript" /></script>
+  <script src="assets/js/main.js" type="text/javascript"></script>
+  <script src="assets/js/partenaires.js" type="text/javascript"></script>
+  <script src="https://unpkg.com/webkul-micron@1.1.6/dist/script/micron.min.js" type="text/javascript"></script>
+</body>
+</html>
